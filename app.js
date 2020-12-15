68067b02f790c6cdd7846612b3639feb6a50108c
Jun Young Jang <junyoungjang@Junui-MacBookPro.local>
2020-12-15 23:02:19 +0900

    reset commit

--- app.js
+++ app.js
@@ -1,30 +1,11 @@
-if (process.env.NODE_ENV !== 'production'){
-    require('dotenv').config()
-}
-
 const express = require('express');
 const mongoose = require('mongoose')
 const nunjucks = require('nunjucks');
 const Post = require('./models/posts')
 const postRouter = require('./routes/posts');
 const methodOverride = require('method-override');
-const bcrypt = require('bcrypt')
-const passport = require('passport')
-const flash = require('express-flash')
-const session = require('express-session')
-const bodyParser = require('body-parser')
+
 const app = express()
-
-app.use(bodyParser.urlencoded({extended:true}))
-
-const initializePassport = require('./passport-config')
-initializePassport(
-    passport,
-    email => users.find(user => user.email === email),
-    id => users.find(user => user.id === id)
-)
-
-const users = []
 
 mongoose.connect('mongodb://localhost/blog', {
     useNewUrlParser:true,
@@ -36,14 +17,6 @@ app.set('view engine','ejs');
 
 
 app.use(express.urlencoded({ extended: false}))
-app.use(flash())
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-}))
-app.use(passport.initialize())
-app.use(passport.session())
 
 app.use(methodOverride('_method'))
 
@@ -55,20 +28,6 @@ app.get('/', async (req,res) => {
 })
 app.use('/posts',postRouter)
 
-app.get('/login',(req,res)=>{
-    res.render('login.ejs')
-})
-
-app.post('/login',(req,res,next)=>{
-    console.log(req.body.email)
-    console.log(req.body.Password)
-    next()
-},
- passport.authenticate('local',{
-    successRedirect:'/',
-    failureRedirect:'/login',
-    failureFlash: true
-}))
 
 
 app.get('/register',(req,res) =>{
@@ -92,24 +51,5 @@ app.post('/register', async (req,res)=>{
     console.log(users)
 })
 
-app.delete('/logout', (req, res) => {
-    req.logOut()
-    res.redirect('/login')
-})
-  
-function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return next()
-    }
-  
-    res.redirect('/login')
-}
-  
-function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-      return res.redirect('/')
-    }
-    next()
-}
 
 app.listen(5000)
